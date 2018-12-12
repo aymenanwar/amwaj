ec7a905b83f80c5d32a7d45d337b7402727d9ad3
aymenanwar <aymen_anwar@hotmail.com>
2018-12-12 18:55:01 +0800

    Updates

---
+++ precache-manifest.b3c5a0bd73bd6fc81a3dd5696982650c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "981e82edef073164c097c69683f7c36b",
+    "url": "/amwaj/static/media/hands.981e82ed.jpeg"
+  },
+  {
+    "revision": "aebb21c724629119503b",
+    "url": "/amwaj/static/css/main.df0e09ae.chunk.css"
+  },
+  {
+    "revision": "2057fb534692c4a049b4d9fceaa975b2",
+    "url": "/amwaj/static/media/amwaj.2057fb53.jpg"
+  },
+  {
+    "revision": "f57c227bab310fda57b2",
+    "url": "/amwaj/static/js/1.f57c227b.chunk.js"
+  },
+  {
+    "revision": "54dc73a309a24df8d96b",
+    "url": "/amwaj/static/js/runtime~main.54dc73a3.js"
+  },
+  {
+    "revision": "ece904f3dc4c865ced9e908babcd4bad",
+    "url": "/amwaj/static/media/startup-photos.ece904f3.jpg"
+  },
+  {
+    "revision": "39bcc39b8af4b5b798636d97befecf87",
+    "url": "/amwaj/static/media/iium.39bcc39b.jpeg"
+  },
+  {
+    "revision": "8c42793da5565a7ff922fbb2395ce02c",
+    "url": "/amwaj/static/media/pexels-photo-1037993.8c42793d.jpeg"
+  },
+  {
+    "revision": "ec5083084cc81556ebe5df949292be05",
+    "url": "/amwaj/static/media/segi.ec508308.jpeg"
+  },
+  {
+    "revision": "43f4867bea87ecd2a32ce7d90a79684c",
+    "url": "/amwaj/static/media/gradu.43f4867b.jpeg"
+  },
+  {
+    "revision": "aebb21c724629119503b",
+    "url": "/amwaj/static/js/main.aebb21c7.chunk.js"
+  },
+  {
+    "revision": "8906596b1f182aa8acab6e0309a7c8ff",
+    "url": "/amwaj/static/media/slide3.8906596b.jpg"
+  },
+  {
+    "revision": "14b7d5afbb6b7c393aa1ec8b5bd07f97",
+    "url": "/amwaj/static/media/hands-people-woman-working.14b7d5af.jpg"
+  },
+  {
+    "revision": "7ac04e37767a4b1ea92637a20b92d700",
+    "url": "/amwaj/static/media/startup.7ac04e37.png"
+  },
+  {
+    "revision": "08893d062dff6daf289a4a9baa8f8573",
+    "url": "/amwaj/static/media/laptop.08893d06.png"
+  },
+  {
+    "revision": "fd3b7e4470180e155c150286609ecf0f",
+    "url": "/amwaj/static/media/meeting.fd3b7e44.png"
+  },
+  {
+    "revision": "137d26cd7ad15a608d19423436deb5ad",
+    "url": "/amwaj/static/media/adults-agenda-cafe-1056551.137d26cd.jpg"
+  },
+  {
+    "revision": "5c324e7a16fdc21e853bade785188e1f",
+    "url": "/amwaj/static/media/slide1.5c324e7a.jpg"
+  },
+  {
+    "revision": "f57c227bab310fda57b2",
+    "url": "/amwaj/static/css/1.c5bcdb5c.chunk.css"
+  },
+  {
+    "revision": "a8190749cffb0dddd143b29c8581257f",
+    "url": "/amwaj/index.html"
+  }
+];
